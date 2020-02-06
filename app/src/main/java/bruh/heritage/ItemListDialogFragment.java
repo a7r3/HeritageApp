3d10f0f6b76e4214726758605aad57de80a9266c
Arvindraj <arvindultimate7352@gmail.com>
2020-02-06 10:42:30 +0530

    Add View Cart Fragment; Some moar modifications
    Signed-off-by: Arvindraj <arvindultimate7352@gmail.com>

---
+++ app/src/main/java/bruh/heritage/ItemListDialogFragment.java
@@ -0,0 +1,108 @@
+package bruh.heritage;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ItemListDialogFragment extends BottomSheetDialogFragment {
+
+    // TODO: Customize parameter argument names
+    private static final String ARG_ITEM_COUNT = "item_count";
+    private Listener mListener;
+
+    // TODO: Customize parameters
+    public static ItemListDialogFragment newInstance(int itemCount) {
+        final ItemListDialogFragment fragment = new ItemListDialogFragment();
+        final Bundle args = new Bundle();
+        args.putInt(ARG_ITEM_COUNT, itemCount);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_item_list_dialog, container, true);
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        setStyle(STYLE_NORMAL, R.style.AppBottomSheetDialogTheme);
+        return super.onCreateDialog(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        final RecyclerView recyclerView = view.findViewById(R.id.items_recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new ItemAdapter(getArguments().getInt(ARG_ITEM_COUNT)));
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        final Fragment parent = getParentFragment();
+        if (parent != null) {
+            mListener = (Listener) parent;
+        } else {
+            mListener = (Listener) context;
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        mListener = null;
+        super.onDetach();
+    }
+
+    public interface Listener {
+        void onItemClicked(int position);
+    }
+
+    private class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    private class ItemAdapter extends RecyclerView.Adapter<ViewHolder> {
+
+        private final int mItemCount;
+
+        ItemAdapter(int itemCount) {
+            mItemCount = itemCount;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(getActivity()).inflate(R.layout.fragment_item_list_dialog_item, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+        }
+
+        @Override
+        public int getItemCount() {
+            return mItemCount;
+        }
+
+    }
+
+}
--- app/src/main/java/bruh/heritage/MainActivity.java
+++ app/src/main/java/bruh/heritage/MainActivity.java
@@ -9,7 +9,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.method.CharacterPickerDialog;
 import android.transition.AutoTransition;
 import android.transition.Transition;
 import android.transition.TransitionManager;
@@ -17,18 +16,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.animation.AccelerateInterpolator;
+import android.widget.ImageView;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ItemListDialogFragment.Listener {
 
     MaterialButton button;
     ConstraintSet loginSet, mainSet;
     ConstraintLayout mainLayout;
     RecyclerView recyclerView;
-    FloatingActionButton floatingActionButton;
+    MaterialButton viewCartButton;
+    private int[] images = {
+            R.drawable.sampleitem,
+            R.drawable.sampleone,
+            R.drawable.sampletwo
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +48,14 @@ public class MainActivity extends AppCompatActivity {
         mainSet.clone(this, R.layout.activity_main_alt);
         button = findViewById(R.id.get_started_button);
 
-        floatingActionButton = findViewById(R.id.view_cart_fab);
+        viewCartButton = findViewById(R.id.view_cart_button);
 
+        viewCartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ItemListDialogFragment.newInstance(2).show(getSupportFragmentManager(), "bruh");
+            }
+        });
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -59,28 +69,23 @@ public class MainActivity extends AppCompatActivity {
         });
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2, RecyclerView.VERTICAL, false));
 
-        recyclerView.setAdapter(new RecyclerView.Adapter() {
-
-            class ViewHolder extends RecyclerView.ViewHolder {
-                public ViewHolder(@NonNull View itemView) {
-                    super(itemView);
-                }
-            }
+        recyclerView.setAdapter(new RecyclerView.Adapter<ViewHolder>() {
 
             @NonNull
             @Override
-            public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
                 return new ViewHolder(LayoutInflater.from(MainActivity.this).inflate(R.layout.grid_item, parent, false));
             }
 
             @Override
-            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
                 holder.itemView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         startActivity(new Intent(MainActivity.this, ViewItemActivity.class));
                     }
                 });
+                holder.image.setImageResource(images[position % 3]);
             }
 
             @Override
@@ -90,4 +95,18 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    public void onItemClicked(int position) {
+
+    }
 }
+
+class ViewHolder extends RecyclerView.ViewHolder {
+    public ImageView image;
+    public ViewHolder(@NonNull View itemView) {
+        super(itemView);
+        image = itemView.findViewById(R.id.item_image);
+    }
+}
+
